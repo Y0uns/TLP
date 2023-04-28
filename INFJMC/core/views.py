0dff988c46a27161e40116e441ef356368bc8b94
Y0uns <edu.jones2002@gmail.com>
2023-04-28 11:54:41 -0400

    Aplicaciones y Templates

--- INFJMC/core/views.py
+++ INFJMC/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    #return HttpResponse("<h1>home<h1>")
+    return render(request, 'core/home.html')
+
+def carreras(request):
+    #return HttpResponse('<h1>carreras<h1>')
+    return render(request, 'core/carreras.html')
+def docentes(request):
+    #return HttpResponse('<h1>docentes<h1>')
+    return render(request, 'core/docentes.html')
+
