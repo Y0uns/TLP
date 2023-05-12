2f94a411f60b82623f564ea502203a2265da89d0
Y0uns <edu.jones2002@gmail.com>
2023-05-12 12:08:37 -0400

    Modelo

--- INFJMC/core/models.py
+++ INFJMC/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Carrera(models.Model):
+    codigo = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=50)
+    duracion = models.IntegerField()
+
+    def __str__(self) :
+        return self.codigo
+    
+class Docente(models.Model):
+    nombre = models.CharField(max_length=10)
+    apellido = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+
+    def __str__(self) :
+        return self.nombre + " "+ self.apellido + " " + self.email  
