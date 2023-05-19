5bfbe92e8758986f643e49748281d6684f539f18
Y0uns <edu.jones2002@gmail.com>
2023-05-19 11:47:07 -0400

    clase2

--- INFJMC/core/models.py
+++ INFJMC/core/models.py
@@ -12,6 +12,6 @@ class Docente(models.Model):
     nombre = models.CharField(max_length=10)
     apellido = models.CharField(max_length=20)
     email = models.CharField(max_length=30)
-
+    
     def __str__(self) :
         return self.nombre + " "+ self.apellido + " " + self.email  
